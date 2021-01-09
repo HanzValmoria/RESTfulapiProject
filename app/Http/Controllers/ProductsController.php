6466cf80e0f5c54319858583946b8738bcebb02e
Hanz Valmoria <valmoriazas143@gmail.com>
2021-01-09 13:27:55 +0800

    RestfulAPI

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Model\Products;
+use Validator;
+class ProductsController extends Controller
+{
+   
+    public function index()
+    {
+ 
+        
+    $products = Products::select("products.*")->get()->toArray();
+
+                return response()->json($products);
+    }
+
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'name' => 'required|unique:products|max:60',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'ok' => false,
+                'error' => $validator->messages(),
+            ]);
+        }
+        try {
+            Products::create($input);
+            return response()->json([
+                "ok" => true,
+                "mensaje" => "Successfully registered!",
+            ]);
+        } catch (\Exception $ex) {
+            return response()->json([
+                "ok" => false,
+                "error" => $ex->getMessage(),
+            ]);
+        }
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $products = Products::select("products.*")
+            ->where("products.id", $id)
+            ->first();
+        return response()->json([
+            "ok" => true,
+           "data" => $products,            
+        ]);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'name' => 'required|max:60',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'ok' => false,
+                'error' => $validator->messages(),
+            ]);
+        }
+        try {
+            $products = Products::find($id);
+            if ($products == false) {
+                return response()->json([
+                    "ok" => false,
+                    "error" => "It was not found",
+                ]);
+            }
+            $products->update($input);
+            return response()->json([
+                "ok" => true,
+                "mensaje" => "It was successfully modified!",
+            ]);
+        } catch (\Exception $ex) {
+            return response()->json([
+                "ok" => false,
+                "error" => $ex->getMessage(),
+            ]);
+        }
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $products = Products::find($id);
+            if ($products == false) {
+                return response()->json([
+                    "ok" => false,
+                    "error" => "
+                    It was not found",
+                ]);
+            }
+            $products->delete([
+            ]);
+            return response()->json([
+                "ok" => true,
+                "mensaje" => "It was successfully removed!",
+            ]);
+        } catch (\Exception $ex) {
+            return response()->json([
+                "ok" => false,
+                "error" => $ex->getMessage(),
+            ]);
+        }
+    }
+}
---
+++ app/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    public $table = "products";
+
+    protected $fillable = [
+        'name', 
+        'price', 
+        'stock'
+    ];
+
+    public $timestamps = false;
+
+}
